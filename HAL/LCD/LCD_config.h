2ff4808687e59fe9d720c0ff36d5ba5efb007335
Ilcendare <Thetr619@gmail.com>
2023-10-21 11:48:27 +0300

    Update

---
+++ HAL/LCD/LCD_config.h
@@ -0,0 +1,21 @@
+/*
+ * LCD_configuration.h
+ *
+ *  Created on: Aug 15, 2023
+ *  Author: ENG. Mahmoud Sayed
+ */
+
+#ifndef HAL_LCD_LCD_CONFIG_H
+#define HAL_LCD_LCD_CONFIG_H
+
+
+#define RS				DIOA, PIN3
+//#define RW				(6)
+#define E				DIOA, PIN2
+#define DB7				DIOB, PIN4
+#define DB6				DIOB, PIN2
+#define DB5				DIOB, PIN1
+#define DB4				DIOB, PIN0
+
+
+#endif /* HAL_LCD_LCD_CONFIG_H_ */
--- main.c
+++ main.c
@@ -14,8 +14,16 @@
 #include "STD_Types.h"
 #include "BIT_Math.h"
 #include <util/delay.h>
-#include "DIO_interface.h"
 #include "HAL/SEVEN_SEG/SEVEN_SEG_interface.h"
+#include "DIO_interface.h"
+#include "LCD_interface.h"
+/** ========================================================================= */
+
+
+
+
+/** ========================== User-defined Macros ========================== */
+
 /** ========================================================================= */
 
 
@@ -39,41 +47,13 @@ void Initialize_System(void);
 int main(void)
 {
 	Initialize_System();
+	HAL_LCD_WriteString((u8 *)"Hello World!", 0, 0);
 
 	while(1)
 	{
-		for(u8 i = 0; i < 10; i++)
-		{
-			HAL_SEVEN_SEG_voidDisplay(COM1,i);
-			_delay_ms(1000);
-		}
 
-		MCAL_DIO_voidWritePin(DIOA,PIN3,High);
-
-		for(u8 i = 0; i < 10; i++)
-		{
-			HAL_SEVEN_SEG_voidDisplay(COM2,i);
-			_delay_ms(1000);
-		}
-
-		MCAL_DIO_voidWritePin(DIOA,PIN2,High);
-
-		for(u8 i = 0; i < 10; i++)
-		{
-			HAL_SEVEN_SEG_voidDisplay(COM3,i);
-			_delay_ms(1000);
-		}
-
-		MCAL_DIO_voidWritePin(DIOB,PIN5,High);
-
-		for(u8 i = 0; i < 10; i++)
-		{
-			HAL_SEVEN_SEG_voidDisplay(COM4,i);
-			_delay_ms(1000);
-		}
-
-		MCAL_DIO_voidWritePin(DIOB,PIN6,High);
 	}
+
 }
 /** ========================================================================= */
 
@@ -90,21 +70,7 @@ int main(void)
 /**   This function contains all the initialization required for the system   */
 void Initialize_System(void)
 {
-	MCAL_DIO_voidSetPinMode(DIOA,PIN3,Output);
-	MCAL_DIO_voidSetPinMode(DIOA,PIN2,Output);
-	MCAL_DIO_voidSetPinMode(DIOB,PIN5,Output);
-	MCAL_DIO_voidSetPinMode(DIOB,PIN6,Output);
-
-	MCAL_DIO_voidWritePin(DIOA,PIN3,High);
-	MCAL_DIO_voidWritePin(DIOA,PIN2,High);
-	MCAL_DIO_voidWritePin(DIOB,PIN5,High);
-	MCAL_DIO_voidWritePin(DIOB,PIN6,High);
-
-	/* Set all the A, B, C, and D pins as Output */
-	MCAL_DIO_voidSetPinMode(DIOB, PIN0, Output); // Set A as output
-	MCAL_DIO_voidSetPinMode(DIOB, PIN1, Output); // Set B as output
-	MCAL_DIO_voidSetPinMode(DIOB, PIN2, Output); // Set C as output
-	MCAL_DIO_voidSetPinMode(DIOB, PIN4, Output); // Set D as output
+	HAL_LCD_Init();
 }
 /** ========================================================================= */
 
